50b9be625473b12223459885f51725a7cd460e12
Sese <sese.miguel9@gmail.com>
2020-07-02 23:05:27 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: "Miguel Karlo Sese",
+      age: 24
+
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age}</h3>
+      </>
+    )
+  }
 }
 
 export default App;
